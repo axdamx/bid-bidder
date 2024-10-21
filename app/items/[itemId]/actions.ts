c5c63d8edd6197453b67c8f9217a10d26a0b5b20
axdamx <axdamx@gmail.com>
2024-10-21 23:19:21 +0800

    implement socket io implementation tracking bids and user counts

--- app/items/[itemId]/actions.ts
+++ app/items/[itemId]/actions.ts
@@ -5,9 +5,10 @@ import { database } from "@/src/db/database";
 import { bids, items } from "@/src/db/schema";
 import { desc, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
-import { broadcastNewBid } from "../../../websocketServer"; // Adjust the path based on your project structure
+import { io } from "../../../websocketServer";
 
 export async function createBidAction(itemId: number) {
+    console.log('here too', itemId)
     const session = await auth();
 
     if (!session || !session.user || !session.user.id) {
@@ -37,25 +38,27 @@ export async function createBidAction(itemId: number) {
         currentBid: latestBidValue,
     }).where(eq(items.id, itemId))
 
-        // Fetch the latest bid with user information
-        const latestBid = await database.query.bids.findFirst({
-            where: eq(bids.itemId, itemId),
-            orderBy: desc(bids.id),
-            with: {
-                user: {
-                    columns: {
-                        image: true,
-                        name: true,
-                    },
+    // Fetch the latest bid with user information
+    const latestBid = await database.query.bids.findFirst({
+        where: eq(bids.itemId, itemId),
+        orderBy: desc(bids.id),
+        with: {
+            user: {
+                columns: {
+                    image: true,
+                    name: true,
                 },
             },
-        });
+        },
+    });
     
-        broadcastNewBid({
-            itemId,
-            newBid: latestBidValue,
-            bidInfo: latestBid
-        });
+    // Emit the new bid to clients
+    console.log('Emitting new bid:', { itemId, newBid: latestBidValue, bidInfo: latestBid });
+    io.emit('newBid', {
+        itemId,
+        newBid: latestBidValue,
+        bidInfo: latestBid,
+    });
 
     revalidatePath(`/items/${itemId}`);
 }  
