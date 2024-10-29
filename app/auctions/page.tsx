4c1a9dea17a81ede0ad0122f3c97ea009cdff646
axdamx <axdamx@gmail.com>
2024-10-29 23:02:52 +0800

    fix card item in auctions page

--- app/auctions/page.tsx
+++ app/auctions/page.tsx
@@ -4,6 +4,7 @@ import { eq } from "drizzle-orm";
 import { items } from "@/src/db/schema";
 import { auth } from "../auth";
 import { EmptyState } from "./empty-state";
+import { getItemsByUserId } from "../profile/[userId]/action";
 
 export default async function MyAuctionPage() {
   const session = await auth();
@@ -16,14 +17,22 @@ export default async function MyAuctionPage() {
     where: eq(items.userId, session.user.id!),
   });
 
-  const hasItems = allItems.length > 0;
+  const { ownedItems, error } = await getItemsByUserId(session.user.id!);
+  if (error) {
+    // Handle error
+  } else {
+    // Use items
+    console.log("ownedItems", ownedItems);
+  }
+
+  const hasItems = ownedItems.length > 0;
 
   return (
     <main className="container mx-auto py-12">
       <h2 className="text-2xl font-bold mb-4">My Current Auctions</h2>
       {hasItems ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
-          {allItems.map((item) => (
+          {ownedItems.map((item) => (
             <ItemCard key={item.id} item={item} />
           ))}
         </div>
