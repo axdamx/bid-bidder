2b289ee597557e8c5836469bad50d75227acb7c5
axdamx <axdamx@gmail.com>
2025-03-14 23:41:41 +0800

    cleanup and add sitemap

--- websocketServer.ts
+++
@@ -1,8 +0,0 @@
-// websocketServer.ts
-import { server } from "./websocket"; // Import the server, which already has the io logic
-
-const PORT = 8082;
-
-server.listen(PORT, () => {
-  console.log(`WebSocket server is running on port ${PORT}`);
-});
